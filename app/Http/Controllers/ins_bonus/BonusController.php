dcaa231c45dce6bdd411c49a867fdbf423eb276d
janecheng <jane.zheng@leishan.com.tw>
2020-02-07 16:51:02 +0800

    revise api call function

--- app/Http/Controllers/ins_bonus/BonusController.php
+++ app/Http/Controllers/ins_bonus/BonusController.php
@@ -42,7 +42,7 @@ class BonusController extends Controller
                 }
                 break;
             case 300000735: //遠雄人壽
-                $array = Farglory::bonus_ori($file, $doc_name, $period, $supplier);
+                $array = Farglory::bonusOri($file, $doc_name, $period, $supplier);
                 break;
             case 300000734: //富邦人壽
                 $array = Fubon::bonusOri($file, $doc_name, $period, $supplier);
