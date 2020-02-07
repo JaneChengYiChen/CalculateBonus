0f02b561454dbf065511ea1c416e973532721d5c
janecheng <jane.zheng@leishan.com.tw>
2020-02-07 10:10:47 +0800

    Import Fubon bonus files format 2

--- app/Http/Controllers/ins_bonus/bonus_from_suppliers_function/Fubon.php
+++ app/Http/Controllers/ins_bonus/bonus_from_suppliers_function/Fubon.php
@@ -11,23 +11,19 @@ class Fubon extends Controller
     {
         $array = array();
 
-        $file = mb_convert_encoding($file, "UTF-8", "BIG5");
-
         foreach (explode("\n", $file) as $file_key => $file_value) {
-            $strlen_detection = \strlen($file_value);
-            switch ($strlen_detection) {
-                case ($strlen_detection > 380):
-                    if ($strlen_detection !== 0) {
-                        $array = self::formatOverStrlen(
-                            $file_value,
-                            $array,
-                            $doc_name,
-                            $period,
-                            $supplier
-                        );
-                    }
+            switch ($doc_name) {
+                case (preg_match('/csv/i', strtolower($doc_name)) == 1):
+                    $array = self::formatCsvFile(
+                        $file_value,
+                        $array,
+                        $doc_name,
+                        $period,
+                        $supplier
+                    );
                     break;
                 default:
+                    $file_value = mb_convert_encoding($file_value, "UTF-8", "BIG5");
                     $array = self::formatDefault(
                         $file_value,
                         $array,
@@ -38,37 +34,28 @@ class Fubon extends Controller
                     break;
             }
         }
-
         return $array;
     }
 
-    private static function formatOverStrlen($file_value, $array, $doc_name, $period, $supplier)
+    private static function formatCsvFile($file_value, $array, $doc_name, $period, $supplier)
     {
-        echo vaR_dump($file_value);
-        exit;
-        $sup_code = substr($file_value, 0, 6); #單位代號
-        $sup_period = substr($file_value, 6, 6); #保單期間
-        $source = substr($file_value, 12, 1); #資料來源；A-壽險;B-個人傷害險;C-旅行險
-        $ins_no = trim(substr($file_value, 13, 14)); #保單號碼
-        $payer_name = trim(substr($file_value, 27, 14)); #要保人姓名
-        $payer_ID = trim(substr($file_value, 94, 12)); #要保人ID
-        $insured_name = trim(substr($file_value, -230, 14)); #被保人姓名
-        $insured_id = trim(substr($file_value, -210, 10)); #被保人ID
-        $bonus_rate = null;
-        $handle_id = substr($file_value, -196, 10); #業務員ID
-        $effe_date = substr($file_value, -186, 8); #生效日
-        $ins_type = substr($file_value, -177, 4); #險種
-        $total_pay_period = substr($file_value, -171, 2); #年期
-        $pay_type = substr($file_value, -169, 1); #繳別
-        $recent_pay_period = (int) substr($file_value, -168, 2); #繳費年度
-        $recent_pay_times = (int) substr($file_value, -166, 2); #繳費次數
-        $premium_ori = (int) substr($file_value, -90, 15); #主約基本保費
-        $premium_twd = (int) substr($file_value, -158, 12);
-        $bonus = (int) substr($file_value, -100, 10); #服務津貼
-        $crc = substr($file_value, -60, 3); #幣別
-        $crc_rate = trim(substr($file_value, -57, 11)); #匯率
-        $occupation = substr($file_value, -46, 1); #職業等級
-        $main_code = substr($file_value, -178, 1);
+        $file_value = explode(',', $file_value);
+        $handle_id = substr($file_value[3], 10, 10);
+        $insured_id = '';
+        $ins_no = substr($file_value[0], 13, 10);
+        $main_code = '';
+        $effe_date = substr($file_value[3], 20, 7);
+        $ins_type = substr($file_value[3], 27, 3);
+        $total_pay_period = substr($file_value[3], 31, 2);
+        $pay_type = substr($file_value[3], 33, 1);
+        $recent_pay_period = substr($file_value[3], 34, 2);
+        $premium_ori = (int) $file_value[5];
+        $premium_twd = (int) $file_value[3];
+        $bonus = (int) $file_value[19];
+        $crc = substr($file_value[23], -3, 3);
+        $crc_rate = (int) $file_value[26];
+        $bonus_rate = '';
+        $recent_pay_times = '';
 
         array_push($array, array(
             "doc_name" => $doc_name,
