e5087e8962fa1fe0cedc63b80c8ce44d9aa3a029
janecheng <jane.zheng@leishan.com.tw>
2020-02-11 17:55:37 +0800

    sending email of bonus diff excel

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Exports\BonusDiffExport;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use Mail;
+
+class MailController extends Controller
+{
+    public function send(Request $request)
+    {
+        $requestBody = $request->json()->all();
+
+        $startPeriod = $requestBody[0]["startPeriod"];
+        $endPeriod = $requestBody[0]["endPeriod"];
+        $supplier = $requestBody[0]["supplier"];
+
+        ini_set("memory_limit", "1000M");
+
+        $fileName = 'bonus_diff.xlsx';
+        $path = Excel::store(new BonusDiffExport(
+            $startPeriod,
+            $endPeriod,
+            $supplier
+        ), $fileName, 'local');
+
+        $pathToFile = storage_path('app') . DIRECTORY_SEPARATOR . $fileName;
+
+        Mail::raw('bonus_diff，密碼為統編', function ($message) use ($pathToFile) {
+            $to = 'jane.zheng@leishan.com.tw';
+
+            $message->to(env("BonusDiffEmail"))
+                ->subject('服務津貼')
+                ->attach($pathToFile);
+        });
+
+        return response()->json(['success!']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,5 @@ Route::group(['prefix' => 'ins_bonus', 'namespace' => 'ins_bonus'], function () 
     Route::post('ins_details_caculation', 'CalculationController@query'); //計算應該要有的pks佣金
     Route::post('bonus_diff', 'DiffController@mapping'); //計算bonus_diff
     Route::post('diffExportExcel', 'ExcelController@export'); //export Excel
+    Route::post('email', 'MailController@send');//send Email
 });
