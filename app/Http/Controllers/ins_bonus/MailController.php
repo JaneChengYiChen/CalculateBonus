dce395e375c4285dbcfed6779c74c140c2b43b10
janecheng <jane.zheng@leishan.com.tw>
2020-02-12 16:51:02 +0800

    switch another way to zip file as sending email

--- app/Http/Controllers/ins_bonus/MailController.php
+++ app/Http/Controllers/ins_bonus/MailController.php
@@ -36,11 +36,7 @@ class MailController extends Controller
         $pathToFile = "/tmp/" . $fileName;
         $zip_path = "/tmp/" . $fileNameZip;
         $password = env("BonusDiffPassword");
-
-        $zip = new \ZipArchive();
-        $zip->open($zip_path, \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
-        $zip->addFile($pathToFile);
-        $zip->close();
+        system("zip -P {$password} {$zip_path} {$pathToFile}");
 
         $content =
             "Dear all,
