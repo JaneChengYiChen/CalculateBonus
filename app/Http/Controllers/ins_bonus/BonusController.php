2cab588201286cec60e0d143cd10eb712c2aee4d
janecheng <jane.zheng@leishan.com.tw>
2020-07-03 15:49:27 +0800

    clean code of supplier rules

--- app/Http/Controllers/ins_bonus/BonusController.php
+++ app/Http/Controllers/ins_bonus/BonusController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\ins_bonus;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ins_bonus\BonusRules\RulesColumn;
 use App\Http\Controllers\ins_bonus\SupplierImport\AIA;
 use App\Http\Controllers\ins_bonus\SupplierImport\Farglory;
 use App\Http\Controllers\ins_bonus\SupplierImport\Fubon;
@@ -13,9 +14,10 @@ use App\Http\Controllers\ins_bonus\SupplierImport\Yuanta;
 use App\Imports\UsersImport;
 use App\import_bonus_doc_rules;
 use App\import_bonus_suppliers;
-use Illuminate\Http\Request;
-use \PhpOffice\PhpSpreadsheet\Shared\Date;
 use File;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use \PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class BonusController extends Controller
 {
@@ -103,207 +105,23 @@ class BonusController extends Controller
 
         $array = array();
         foreach ($datas[0] as $file_key => $file_value) {
-            if (count($file_value) > 40 
+            if (count($file_value) > 40
                 && empty($file_value[3]) == false
                 && mb_strlen($file_value[3], "UTF-8") == strlen($file_value[3])
-                ) {
+            ) {
                 $data = $file_value;
-                //currency
-                $AUD = 1;
-                $CAD = 1;
-                $EUR = 1;
-                $GBP = 1;
-                $HKD = 1;
-                $JPY = 1;
-                $NTD = 1;
-                $NZD = 1;
-                $RMB = 1;
-                $USD = 1;
-                $ZAR = 1;
+                $RulesColumn = new RulesColumn($file_value);
+                $ColumnModel = $RulesColumn->getReturn();
+                $ColumnModel['doc_name'] = $this->doc_name;
+                $ColumnModel['supplier_code'] = $this->supplier;
 
-                if ($data[10] == '外') {
-                    $NTD = 0;
-                }
-                if ($data[10] == '美') {
-                    $AUD = 0;
-                    $CAD = 0;
-                    $EUR = 0;
-                    $GBP = 0;
-                    $HKD = 0;
-                    $JPY = 0;
-                    $NTD = 0;
-                    $NZD = 0;
-                    $RMB = 0;
-                    $USD = 1;
-                    $ZAR = 0;
-                }
-
-                //period_rules
-                if ($data[18] === 0) {
-                    $data[18] = "全";
-                }
-
-                if ($data[14] === 0 && $data[14] !== '躉') {
-                    $data[14] = "全";
-                }else if($data[14] == '彈性'){
-                    $data[14] = "彈性";
-                }
-
-                $rule_types = ['NNN', 'AAA', 'BBB', 'CCC'];
-
-                $empty_detecter = array();
-                $rules_types_detecter = array();
-                foreach ($rule_types as $rule_types) {
-                    $start_period = array_keys($data, $rule_types);
-                    array_push($empty_detecter, $start_period);
-
-                    if (!empty($start_period)) {
-                        $arr_num = (int) count($start_period) - 1;
-                        $rules_start_period = $start_period[$arr_num] - 20;
-                        $rules_types_insert = $rule_types;
-                        if ($rules_start_period > 0) {
-                            array_push($rules_types_detecter, array(
-                                'rules_start_period' => $rules_start_period,
-                                'rules_types_insert' => $rules_types_insert,
-                            ));
-                        }
-                    }
-                }
-
-                if (count(array_filter($empty_detecter)) == 0) {
-                    $rules_start_period = 0;
-                    $rules_types_insert = 'NNN';
-                } else {
-                    $rules_start_period = $rules_types_detecter[0]["rules_start_period"];
-                    $rules_types_insert = $rules_types_detecter[0]["rules_types_insert"];
-                }
-
-                //product_code
-                $product_code_pre = $data[7];
-                switch ($product_code_pre) {
-                    case ($product_code_pre == '除'):
-                        $product_code = 'exception';
-                        break;
-                    case ($product_code_pre == '全'):
-                        $product_code = 'all_pro';
-                        break;
-                    default:
-                        $product_code = $data[8];
-                        break;
-                }
-
-                $first_period = ($data[21] == "-") ? '0' : $data[21];
-
-                $pay_type = ($data[14] == '躉') ? 'D' : 'all';
-                $is_main = ($data[9] == '附約') ? '0' : 'all';
-                if ($data[14] == '躉') {
-                    $D = 1;
-                    $M = 0;
-                    $Q = 0;
-                    $S = 0;
-                    $Y = 0;
-                } else {
-                    $D = 1;
-                    $M = 1;
-                    $Q = 1;
-                    $S = 1;
-                    $Y = 1;
-                }
-                $y_period_lower_limit = (empty($data[14]) || $data[14] == '全') ? 0 : $data[14];
-                $y_period_upper_limit = (empty($data[15])) ? 99 : $data[15];
-
-                $doc_date_pre = is_null($data[0]) ? null : Date::excelToDateTimeObject($data[0]);
-                $doc_date = is_null($data[0]) ? null : $doc_date_pre->format('Y-m-d');
-
-                $rules_start_date_pre = is_null($data[3]) ? null : Date::excelToDateTimeObject($data[3]);
-                $rules_start_date = is_null($data[3]) ? null : $rules_start_date_pre->format('Y-m-d');
-
-                $rules_due_date_pre = is_null($data[4]) ? null : Date::excelToDateTimeObject($data[4]);
-                $rules_due_date = is_null($data[4]) ? null : $rules_start_date_pre->format('Y-m-d');
-
-                $benefit_lower_limit = ($data[16] == '全') ? 0 : $data[16];
-                $benefit_upper_limit = ($data[17] == '全') ? 0 : $data[17];
-
-                $insured_age_lower_limit = ($data[18] == '全' || is_null($data[19])) ? 0 : $data[18];
-                $insured_age_upper_limit = ($data[19] == '全' || is_null($data[19])) ? 99 : $data[19];
-
-                array_push($array, array(
-                    "doc_name" => $this->doc_name,
-                    "doc_date" => $doc_date,
-                    "doc_number" => $data[1],
-                    "doc_number_leishan" => $data[2],
-                    "rules_start_date" => $rules_start_date,
-                    "rules_due_date" => $rules_due_date,
-                    "auto_extension" => $data[5],
-                    "supplier_code" => $this->supplier,
-                    "supplier_name" => $data[6],
-                    "product_name" => $data[7],
-                    "product_code" => $product_code,
-                    "pro_keyword" => $data[9],
-                    "currency" => $data[10],
-                    "main_keyword1" => $data[11],
-                    "main_keyword2" => $data[12],
-                    "is_same_as_main_period" => $data[13],
-                    "y_period_lower_limit" => $y_period_lower_limit,
-                    "y_period_upper_limit" => $y_period_upper_limit,
-                    "benefit_lower_limit" => $benefit_lower_limit,
-                    "benefit_upper_limit" => $benefit_upper_limit,
-                    "insured_age_lower_limit" => $insured_age_lower_limit,
-                    "insured_age_upper_limit" => $insured_age_upper_limit,
-                    "pay_type" => $pay_type,
-                    "is_main" => $is_main,
-                    "premium_type" => $data[20],
-                    "rules_type" => $rules_types_insert,
-                    "rules_start_period" => $rules_start_period,
-                    "0" => '0',
-                    "1" => $first_period,
-                    "2" => $data[22],
-                    "3" => $data[23],
-                    "4" => $data[24],
-                    "5" => $data[25],
-                    "6" => $data[26],
-                    "7" => $data[27],
-                    "8" => $data[28],
-                    "9" => $data[29],
-                    "10" => $data[30],
-                    "11" => $data[31],
-                    "12" => $data[32],
-                    "13" => $data[33],
-                    "14" => $data[34],
-                    "15" => $data[35],
-                    "16" => $data[36],
-                    "17" => $data[37],
-                    "18" => $data[38],
-                    "19" => $data[39],
-                    "20" => $data[40],
-                    "remark" => $data[42],
-                    "created_at" => date('Y-m-d H:i:s'),
-                    "created_by" => "jane",
-                    "deleted_at" => null,
-                    "deleted_by" => null,
-                    "D" => $D,
-                    "M" => $M,
-                    "Q" => $Q,
-                    "S" => $S,
-                    "Y" => $Y,
-                    "AUD" => $AUD,
-                    "CAD" => $CAD,
-                    "EUR" => $EUR,
-                    "GBP" => $GBP,
-                    "HKD" => $HKD,
-                    "JPY" => $JPY,
-                    "NTD" => $NTD,
-                    "NZD" => $NZD,
-                    "RMB" => $RMB,
-                    "USD" => $USD,
-                    "ZAR" => $ZAR,
-                ));
+                array_push($array, $ColumnModel);
             }
         }
         //將先前的規則改為delete
         import_bonus_doc_rules::where('supplier_code', $this->supplier)
             ->where('deleted_at', null)
-            ->update(['deleted_at' => date('Y-m-d H:i:s'), "deleted_by" => "jane"]);
+            ->update(['deleted_at' => date('Y-m-d H:i:s'), "deleted_by" => Auth::guard('api')->user()->name]);
 
         //新增新的規則
         import_bonus_doc_rules::insert($array);
---
+++ app/Http/Controllers/ins_bonus/BonusRules/RulesColumn.php
@@ -0,0 +1,251 @@
+<?php
+namespace App\Http\Controllers\ins_bonus\BonusRules;
+
+use Illuminate\Support\Facades\Auth;
+use \PhpOffice\PhpSpreadsheet\Shared\Date;
+
+class RulesColumn
+{
+    private $data;
+    private $currency;
+    private $date;
+    private $rules_date;
+    private $rules_due_date;
+    private $first_period;
+
+    public function __construct($columns)
+    {
+        $this->data = $columns;
+        $this->date = $this->data[0];
+        $this->doc_number = $this->data[1];
+        $this->doc_number_leishan = $this->data[2];
+        $this->rules_date = $this->data[3];
+        $this->rules_due_date = $this->data[4];
+        $this->auto_extension = $this->data[5];
+        $this->supplier_name = $this->data[6];
+        $this->product_name = $this->data[7];
+        $this->product_code = $this->data[8];
+        $this->pro_keyword = $this->data[9];
+        $this->currency = $this->data[10];
+        $this->main_keyword1 = $this->data[11];
+        $this->main_keyword2 = $this->data[12];
+        $this->is_same_as_main_period = $this->data[13];
+        $this->y_period_lower_limit = $this->data[14];
+        $this->y_period_upper_limit = $this->data[15];
+        $this->benefit_lower_limit = $this->data[16];
+        $this->benefit_upper_limit = $this->data[17];
+        $this->insured_age_lower_limit = $this->data[18];
+        $this->insured_age_upper_limit = $this->data[19];
+        $this->premium_type = $this->data[20];
+        $this->remark = $this->data[42];
+        $this->doc_date = null;
+        $this->rules_start_date = null;
+        $this->rules_due_date = null;
+        $this->created_at = date('Y-m-d H:i:s');
+        $this->created_by = Auth::guard('api')->user()->name;
+        $this->deleted_at = null;
+        $this->deleted_by = null;
+        $this->{0} = '0';
+        $this->{1} = $this->data[21];
+        $this->{2} = $this->data[22];
+        $this->{3} = $this->data[23];
+        $this->{4} = $this->data[24];
+        $this->{5} = $this->data[25];
+        $this->{6} = $this->data[26];
+        $this->{7} = $this->data[27];
+        $this->{8} = $this->data[28];
+        $this->{9} = $this->data[29];
+        $this->{10} = $this->data[30];
+        $this->{11} = $this->data[31];
+        $this->{12} = $this->data[32];
+        $this->{13} = $this->data[33];
+        $this->{14} = $this->data[34];
+        $this->{15} = $this->data[35];
+        $this->{16} = $this->data[36];
+        $this->{17} = $this->data[37];
+        $this->{18} = $this->data[38];
+        $this->{19} = $this->data[39];
+        $this->{20} = $this->data[40];
+    }
+
+    public function getReturn()
+    {
+        $this->setCurrency();
+        $this->setPeriodRules();
+        $this->setPayType();
+        $this->setPeriodLimit();
+        $this->setRuleStartPeriod();
+        $this->setProductCode();
+        $this->setFirstPeriod();
+        $this->setIsMain();
+        $this->setDate($this->date, $this->doc_date);
+        $this->setDate($this->rules_date, $this->rules_start_date);
+        $this->setDate($this->rules_due_date, $this->rules_due_date);
+        $this->setBenefitLimit();
+        return json_decode(json_encode($this), true);
+    }
+
+    private function setCurrency()
+    {
+        $this->AUD = 0;
+        $this->CAD = 0;
+        $this->EUR = 0;
+        $this->GBP = 0;
+        $this->HKD = 0;
+        $this->JPY = 0;
+        $this->NTD = 0;
+        $this->NZD = 0;
+        $this->RMB = 0;
+        $this->USD = 0;
+        $this->ZAR = 0;
+
+        switch ($this->currency) {
+            case '外':
+                $this->AUD = 1;
+                $this->CAD = 1;
+                $this->EUR = 1;
+                $this->GBP = 1;
+                $this->HKD = 1;
+                $this->JPY = 1;
+                $this->NZD = 1;
+                $this->RMB = 1;
+                $this->USD = 1;
+                $this->ZAR = 1;
+                break;
+            case '美':
+                $this->USD = 1;
+                break;
+            default:
+                $this->AUD = 1;
+                $this->CAD = 1;
+                $this->EUR = 1;
+                $this->GBP = 1;
+                $this->HKD = 1;
+                $this->JPY = 1;
+                $this->NTD = 1;
+                $this->NZD = 1;
+                $this->RMB = 1;
+                $this->USD = 1;
+                $this->ZAR = 1;
+                break;
+        }
+    }
+
+    private function setPeriodRules()
+    {
+        if ($this->insured_age_lower_limit == 0 ||
+            is_null($this->insured_age_upper_limit)) {
+            $this->insured_age_lower_limit = 0;
+            $this->insured_age_upper_limit = 99;
+        }
+    }
+
+    private function setPayType()
+    {
+        if ($this->y_period_lower_limit === 0 && $this->y_period_lower_limit !== '躉') {
+            $this->y_period_lower_limit = "全";
+        } elseif ($this->y_period_lower_limit == '彈性') {
+            $this->y_period_lower_limit = "彈性";
+        }
+
+        $this->pay_type = ($this->y_period_lower_limit == '躉') ? 'D' : 'all';
+
+        if ($this->y_period_lower_limit == '躉') {
+            $this->D = 1;
+            $this->M = 0;
+            $this->Q = 0;
+            $this->S = 0;
+            $this->Y = 0;
+        } else {
+            $this->D = 1;
+            $this->M = 1;
+            $this->Q = 1;
+            $this->S = 1;
+            $this->Y = 1;
+        }
+    }
+
+    private function setPeriodLimit()
+    {
+        $this->y_period_lower_limit = (empty($this->y_period_lower_limit) || $this->y_period_lower_limit == '全')
+        ? 0 : $this->y_period_lower_limit;
+        $this->y_period_upper_limit = (empty($this->y_period_upper_limit)) ? 99 : $this->y_period_upper_limit;
+    }
+
+    private function setRuleStartPeriod()
+    {
+        $rule_types = ['NNN', 'AAA', 'BBB', 'CCC'];
+
+        $empty_detecter = array();
+        $rules_types_detecter = array();
+        foreach ($rule_types as $rule_types) {
+            $start_period = array_keys($this->data, $rule_types);
+            array_push($empty_detecter, $start_period);
+
+            if (!empty($start_period)) {
+                $arr_num = (int) count($start_period) - 1;
+                $rules_start_period = $start_period[$arr_num] - 20;
+                $rules_types_insert = $rule_types;
+                if ($rules_start_period > 0) {
+                    array_push($rules_types_detecter, array(
+                        'rules_start_period' => $rules_start_period,
+                        'rules_types_insert' => $rules_types_insert,
+                    ));
+                }
+            }
+        }
+
+        if (count(array_filter($empty_detecter)) == 0) {
+            $this->rules_start_period = 0;
+            $this->rules_type = 'NNN';
+        } else {
+            $this->rules_start_period = $rules_types_detecter[0]["rules_start_period"];
+            $this->rules_type = $rules_types_detecter[0]["rules_types_insert"];
+        }
+    }
+
+    private function setProductCode()
+    {
+        switch ($this->product_name) {
+            case '除':
+                $this->product_code = 'exception';
+                break;
+            case '全':
+                $this->product_code = 'all_pro';
+                break;
+        }
+    }
+
+    private function setFirstPeriod()
+    {
+        if ($this->{1} == "-") {
+            $this->{1} = '0';
+        }
+    }
+
+    private function setIsMain()
+    {
+        $this->is_main = ($this->pro_keyword == '附約') ? '0' : 'all';
+    }
+
+    private function setDate($originalVar, &$returnVar)
+    {
+        if (is_null($originalVar)) {
+            $returnVar = null;
+            return;
+        }
+
+        $returnVar = Date::excelToDateTimeObject($originalVar)->format('Y-m-d');
+    }
+
+    private function setBenefitLimit()
+    {
+        if ($this->benefit_lower_limit == '全') {
+            $this->benefit_lower_limit = 0;
+        }
+
+        if ($this->benefit_upper_limit == '全') {
+            $this->benefit_upper_limit = 0;
+        }
+    }
+}
